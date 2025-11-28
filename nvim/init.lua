3889def686fcd0d30ffe6be74df985690345ef23
PioterDev <pimikotr@gmail.com>
2025-11-28 20:06:19 +0100

    Cleanup init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,33 +6,18 @@ require("config.tree-sitter")
 require("config.options")
 require("config.keybinds")
 
-vim.cmd("source " .. vim.fn.stdpath('config') .. "/start.vim")
-vim.cmd("source " .. vim.fn.stdpath('config') .. "/colors/default.vim")
+vim.cmd("source " .. vim.fn.stdpath("config") .. "/start.vim")
+vim.cmd("source " .. vim.fn.stdpath("config") .. "/colors/default.vim")
 
-require'nvim-treesitter.configs'.setup {
-  -- ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+require("nvim-treesitter.configs").setup {
+	sync_install = false,
+	auto_install = false,
+	ignore_install = {  },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
-
-  ignore_install = {  },
-
-  highlight = {
-    enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-	-- disable = { "c", "cpp" },
-  },
-  filetypes = {
-	-- c = { "c", "h" },
-	-- cpp = { "cpp", "hpp" },
-  }
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	filetypes = {}
 }
 require("toggleterm").setup{}
